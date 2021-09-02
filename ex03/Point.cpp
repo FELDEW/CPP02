505a3698f861208226d1b2d4e159a04d620da441
Hazel Buck <hbuck@il-i3.msk.21-school.ru>
2021-09-02 15:53:18 +0300

    made ex03

---
+++ ex03/Point.cpp
@@ -0,0 +1,39 @@
+#include "Point.hpp"
+
+Point::Point():x(0), y(0)
+{
+
+}
+
+Point::~Point()
+{
+
+}
+
+
+
+Point::Point(const Point& origin): x(origin.getX()), y(origin.getY())
+{
+
+}
+
+Point::Point(float const newx, float const newy):x(newx), y(newy)
+{
+
+}
+
+Fixed const Point::getX() const
+{
+	return(this->x);
+}
+
+Fixed const Point::getY() const
+{
+	return(this->y);
+}
+
+Point& Point::operator=(const Point& origin)
+{
+	origin.getX();
+	return (*this);
+}
---
+++ ex03/Point.hpp
@@ -0,0 +1,25 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include <iostream>
+#include <cmath>
+#include "Fixed.hpp"
+
+class Point
+{
+	public:
+		Point();
+		~Point();
+		Point(const Point& origin);
+		Point(const float x, const float y);
+		Point& operator=(const Point& origin);
+		Fixed const getX() const;
+		Fixed const getY() const;
+	private:
+		Fixed const x;
+		Fixed const y;
+};
+
+bool bsp( Point const a, Point const b, Point const c, Point const point);
+
+#endif
